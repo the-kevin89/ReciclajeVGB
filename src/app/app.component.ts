6db8fe6799917b4f3799e8f191ba809f53eb73b4
Kevin Lucio Pérez Palau <klperezpalau@escuelasproa.edu.ar>
2023-06-24 17:11:43 -0300

    Creación de la página "Inicio"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReciclajeVGB';
+
+  isMenuOpen: boolean = false;
+
+  openMenu() {
+    this.isMenuOpen = true;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
