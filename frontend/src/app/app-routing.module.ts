7080164e2f3e8e89a94c122e9e94c75b8ed41206
Kevin Lucio Pérez Palau <klperezpalau@escuelasproa.edu.ar>
2023-07-09 16:20:56 -0300

    Enrutado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ProblemaComponent } from './pages/problema/problema.component';
+import { AreasContaminadasComponent } from './pages/areas-contaminadas/areas-contaminadas.component';
+import { ReciclarBasuraComponent } from './pages/reciclar-basura/reciclar-basura.component';
+import { MotivarAyudarComponent } from './pages/motivar-ayudar/motivar-ayudar.component';
+import { ForoIdeasComponent } from './pages/foro-ideas/foro-ideas.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { IniciarSesionComponent } from './pages/iniciar-sesion/iniciar-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'problema', component: ProblemaComponent },
+  { path: 'areas-contaminadas', component: AreasContaminadasComponent },
+  { path: 'reciclar-basura', component: ReciclarBasuraComponent },
+  { path: 'motivar-ayudar', component: MotivarAyudarComponent },
+  { path: 'foro-ideas', component: ForoIdeasComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'iniciar-sesion', component: IniciarSesionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
